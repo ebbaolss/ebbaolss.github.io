f788ffb0dd173a91b6d871b2c2a23e737d8fceb5
Ebba Maja Olsson <ebbamaja1@gmail.com>
2024-09-12 01:55:45 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "ebbaolss.github.io", // Adjust this if your repository name is different
+  build: {
+    outDir: "build", // Ensure the build directory is correct
+  },
 });
