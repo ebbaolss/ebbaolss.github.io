15fcbb67c91d245944907100c72a7c7af24879a6
Ebba Maja Olsson <ebbamaja1@gmail.com>
2024-09-17 00:29:50 -0700

    Deploy fix

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,24 +1,5 @@
-import logo from "./logo.svg";
-import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
 
 import profile from "./profile.jpg";
 import berkeley from "./company/berkeley.svg";
@@ -36,26 +17,8 @@ import github from "./github.png";
 import portfolio from "./portfolio-project.png";
 
 export {
-  logo,
-  backend,
-  creator,
-  mobile,
-  web,
   menu,
   close,
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
   profile,
   berkeley,
   vipps,
